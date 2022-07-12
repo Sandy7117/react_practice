686267be28a71aee5a8dedeed426467d55ecbb3b
Sandeep <sandeepsonu7117517@gmail.com>
2022-07-12 12:14:32 +0530

    added div for name of character

--- src/Components/MainSection/MainSection.js
+++ src/Components/MainSection/MainSection.js
@@ -20,11 +20,11 @@ const MainSection = () => {
       <h1 className="main_heading_style">Selected Filters</h1>
       <p style={{ marginBottom: "2px" }}>Search by Name</p>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <span>
+        <div>
           <input type="text" id="search" />
           <button>Search</button>
-        </span>
-        <span>
+        </div>
+        <div>
           <form>
             <select id="sort_by" name="options">
               <option defaultValue="sort_by">Sort by ID</option>
@@ -33,19 +33,30 @@ const MainSection = () => {
             </select>
             <input type="submit" value="Submit" />
           </form>
-        </span>
+        </div>
       </div>
       <hr />
       <div className="content_area">
         {!loading &&
           fetchedData.map((characters) => (
-            <span key={characters.id} className="second_half">
-              <img
-                style={{ borderRadius: "5px" }}
-                width="240px"
-                src={characters.image}
-                alt=""
-              />
+            <div key={characters.id} className="second_half">
+              <div>
+                <img
+                  style={{ borderRadius: "5px" }}
+                  width="240px"
+                  src={characters.image}
+                  alt=""
+                />
+                <div className="bottom-left">
+                  <p style={{ fontSize: "15px", paddingLeft: "10px" }}>
+                    {characters.name}
+                  </p>
+                  <p style={{ fontSize: "10px", paddingLeft: "10px" }}>
+                    id: {characters.id} - created {characters.created}
+                  </p>
+                </div>
+              </div>
+
               <div className="character_details">
                 <div className="sub_details">
                   <p>STATUS</p>
@@ -72,7 +83,7 @@ const MainSection = () => {
                   <p style={{ color: "orange" }}>{characters.location.name}</p>
                 </div>
               </div>
-            </span>
+            </div>
           ))}
       </div>
     </div>
