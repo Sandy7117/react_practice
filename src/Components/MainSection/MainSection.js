ed2e79f3c1dd89d4b4fa480393b602d387714092
Sandeep <sandeepsonu7117517@gmail.com>
2022-07-12 14:06:06 +0530

    added pagination

--- src/Components/MainSection/MainSection.js
+++ src/Components/MainSection/MainSection.js
@@ -1,38 +1,99 @@
 import React, { useEffect, useState } from "react";
 import "./MainSection.css";
-import axios from "axios";
+import Axios from "axios";
 
 const MainSection = () => {
   const [fetchedData, setFetchedData] = useState([]);
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    axios.get(`https://rickandmortyapi.com/api/character/`).then((res) => {
-      setLoading(false);
+  const [nextUrl, setNextUrl] = useState("");
+  const [previousUrl, setPreviousUrl] = useState("");
+  const [totalPages, setTotalPages] = useState("");
+  const [devicesPage, setDevicesPage] = useState(1);
+
+  const fetchCharacters = (url) => {
+    if (!url) {
+      url = `https://rickandmortyapi.com/api/character/`;
+    }
+    Axios({
+      method: "get",
+      url: url,
+    }).then((res) => {
       const persons = res.data;
       const data = persons.results;
       setFetchedData(data);
-      console.log(persons);
+      setTotalPages(persons.info.pages);
+      setNextUrl(persons.info.next);
+      setPreviousUrl(persons.info.prev);
+      setLoading(false);
     });
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+    setDevicesPage(1);
   }, []);
+
+  const goToPreviousHandler = () => {
+    fetchCharacters(previousUrl);
+    setDevicesPage(devicesPage - 1);
+  };
+
+  const goToNextHandler = () => {
+    fetchCharacters(nextUrl);
+    setDevicesPage(devicesPage + 1);
+  };
 
   return (
     <div className="main_section_style">
       <h1 className="main_heading_style">Selected Filters</h1>
       <p style={{ marginBottom: "2px" }}>Search by Name</p>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          textAlign: "center",
+        }}
+      >
+        <div style={{ display: "flex" }}>
           <input type="text" id="search" />
           <button>Search</button>
         </div>
-        <div>
-          <form>
-            <select id="sort_by" name="options">
-              <option defaultValue="sort_by">Sort by ID</option>
-              <option value="ascending">Ascending</option>
-              <option value="descending">Descending</option>
-            </select>
-            <input type="submit" value="Submit" />
-          </form>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "self-end",
+          }}
+        >
+          <div>
+            <form>
+              <select id="sort_by" name="options">
+                <option defaultValue="sort_by">Sort by ID</option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+              </select>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+          <div className="pagination-div">
+            {devicesPage > 1 ? (
+              <button onClick={goToPreviousHandler} className="btn">
+                Prev
+              </button>
+            ) : (
+              ""
+            )}
+            <p className="current-page">
+              Page {devicesPage} of {totalPages}
+            </p>
+            {devicesPage < totalPages ? (
+              <button onClick={goToNextHandler} className="btn">
+                Next
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
       <hr />
